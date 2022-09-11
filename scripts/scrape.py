e61c4bccd90cd136f284c8c12269614506a8cf1f
shiliu2 <iris@LAPTOP-HRTEB95R.localdomain>
2022-09-11 17:22:53 +0800

    message

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,136 +1,164 @@
-"""
-A very simple and basic web scraping script. Feel free to
-use this as a source of inspiration, but, make sure to attribute
-it if you do so.
+#modified the skeleton code provided
 
-This is by no means production code.
-"""
 # built-in imports
 import re
 import requests
 import numpy as np
+import pandas as pd
 from json import dump
 from ast import excepthandler
 from collections import defaultdict
+
 
 # user packages
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-# constants
+#create a dataframe to store url links for each postcode
+postcode_urls = pd.DataFrame(columns=['postcode','url_links'])
 BASE_URL = "https://www.domain.com.au"
-N_PAGES = range(1, 51) # maximum 50 pages
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0)"
             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-# begin code
-url_links = []
-property_metadata = defaultdict(dict)
 
+#VIC postcodes are from 3000 to 4000
+postcodes = range(3000,4000)
 
+for postcode in postcodes:
+    url_links = []
+    postcode_url = f"https://www.domain.com.au/rent/?postcode={postcode}&ssubs=0&sort=default-desc"
+    postcode_bs = BeautifulSoup(requests.get(postcode_url, headers=headers).text, "html.parser")
 
-# generate list of urls to visit
-for page in N_PAGES:
-    #change ptype=house/apartment/town-house to get properties of these three types
-    url = BASE_URL + f"/rent/?ptype=town-house&sort=default-desc&state=vic&page={page}"  
-    bs_object = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
-
-
-    # find the unordered list (ul) elements which are the results, then
-    # find all href (a) tags that are from the base_url website.
-    index_links = bs_object \
-        .find(
-            "ul",
-            {"data-testid": "results"}
-        ) \
-        .findAll(
-            "a",
-            href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
+    #get number of rental properties for each postcode
+    num_results = int(re \
+        .search(r'\d+',postcode_bs \
+        .find('h1',{"data-testid":"summary"}) \
+        .find("strong").text) \
+        .group()
         )
-
-    for link in index_links:
-        # if its a property address, add it to the list
-        if 'address' in link['class']:
-            url_links.append(link['href'])
-
-
-# for each url, scrape some basic metadata
-
-for property_url in url_links[1:]:
-    bs_object = BeautifulSoup(requests.get(property_url, headers=headers).text, "html.parser")
-
-    # looks for the header class to get property name
-    property_metadata[property_url]['name'] = bs_object \
-        .find("h1", {"class": "css-164r41r"}) \
-        .text
-
-    # looks for the div containing a summary title for cost
-    property_metadata[property_url]['cost_text'] = bs_object \
-        .find("div", {"data-testid": "listing-details__summary-title"}) \
-        .text
-
-    # extract coordinates from the hyperlink provided
-    property_metadata[property_url]['coordinates'] = [
-        float(coord) for coord in re.findall(
-            r'destination=([-\s,\d\.]+)', # use regex101.com here if you need to
-            bs_object \
+    #check if it has results, and calculate the corresponding pages it needed to display
+    #one page can only display 20 results
+    if num_results != 0:
+        if num_results % 20 == 0:
+            N_PAGES = range(1,num_results//20 + 1)        
+        else:
+            N_PAGES = range(1,num_results//20 + 2)
+        
+        #generate list of urls to visit for each postcode
+        for page in N_PAGES:
+            url = postcode_url + f"&page={page}"  
+            bs_object = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
+            
+            # find the unordered list (ul) elements which are the results, then
+            # find all href (a) tags that are from the base_url website.
+            index_links = bs_object \
                 .find(
-                    "a",
-                    {"target": "_blank", 'rel': "noopener noreferer"}
+                    "ul",
+                    {"data-testid": "results"}
                 ) \
-                .attrs['href']
-        )[0].split(',')
-    ]
+                .findAll(
+                    "a",
+                    href=re.compile(f"{BASE_URL}/*") # the `*` denotes wildcard any
+                )
 
-    #extract number of beds, number of bathrooms and number of parking
-    property_metadata[property_url]['rooms'] = [
-        re.findall(r'\d\s[A-Za-z]+', feature.text) for feature in bs_object \
-            .find("div", {"data-testid": "property-features"}) \
-            .findAll("span", {"data-testid": "property-features-text-container"})
-    ]
+            for link in index_links:
+                # if its a property address, add it to the list
+                if 'address' in link['class']:
+                    url_links.append(link['href'])
 
-    #extract internal area (in m^2) 
-    try:
-        property_metadata[property_url]['area'] = re \
-            .search(r"\d+",bs_object \
-            .find("div", {"data-testid": "strip-content-list"}) \
-            .find("span").text) \
-            .group()
-    except:
-        property_metadata[property_url]['area'] = np.NaN
+        #store list of urls with corresponding postcode into the dataframe
+        postcode_urls.loc[len(postcode_urls)] = [postcode,url_links]
 
 
-    #extract property type
-    try:
-        property_metadata[property_url]['type'] = bs_object \
-                .find("div",{"data-testid": "listing-summary-property-type"}) \
-                .find("span") \
+
+# for each url of each postcode, scrape some basic metadata
+property_metadata = defaultdict(dict)
+for i in range(len(postcode_urls)):
+    
+    for property_url in postcode_urls["url_links"][i][:]: 
+        bs_object = BeautifulSoup(requests.get(property_url, headers=headers).text, "html.parser")
+
+        #check if the page has valid information
+        if bs_object.find("h1", {"class": "css-164r41r"}):
+            # looks for the header class to get property name
+            property_metadata[property_url]['name'] = bs_object \
+                .find("h1", {"class": "css-164r41r"}) \
                 .text
-    except:
-        property_metadata[property_url]['type'] = np.NaN
+            
+            # looks for the div containing a summary title for cost
+            property_metadata[property_url]['cost_text'] = bs_object \
+                .find("div", {"data-testid": "listing-details__summary-title"}) \
+                .text
 
-    #extract rental bond($)
-    try:
-        property_metadata[property_url]["bond"] = re \
-            .search(r"\d+",bs_object \
-            .find("div",{"data-testid":"strip-content-list"}) \
-            .findAll("li")[1].text) \
-            .group()
-    except :
-        property_metadata[property_url]["bond"] = np.NaN
+            # extract coordinates from the hyperlink provided
+            property_metadata[property_url]['coordinates'] = [
+                float(coord) for coord in re.findall(
+                    r'destination=([-\s,\d\.]+)', 
+                    bs_object \
+                        .find(
+                            "a",
+                            {"target": "_blank", 'rel': "noopener noreferer"}
+                        ) \
+                        .attrs['href']
+                )[0].split(',')
+            ]
 
-    #extract property features
-    property_metadata[property_url]['features'] = [
-        feature.text for feature in bs_object \
-            .findAll("li",{"data-testid": "listing-details__additional-features-listing"}) \
-    ]
-  
-    #extract property description
-    property_metadata[property_url]['desc'] = re \
-        .sub(r'<br\/>', '\n', str(bs_object.find("p"))) \
-        .strip('</p>')
+            #extract number of rooms, number of bathrooms and number of parking
+            property_metadata[property_url]['rooms'] = [
+                re.findall(r'\d\s[A-Za-z]+', feature.text) for feature in bs_object \
+                    .find("div", {"data-testid": "property-features"}) \
+                    .findAll("span", {"data-testid": "property-features-text-container"})
+            ]
+
+            #extract internal area (in m^2) 
+            try:
+                property_metadata[property_url]['area'] = re \
+                    .search(r"\d+",bs_object \
+                    .find("div", {"data-testid": "strip-content-list"}) \
+                    .find("span").text) \
+                    .group()
+            except:
+                property_metadata[property_url]['area'] = np.NaN
+
+
+            #extract property type
+            try:
+                property_metadata[property_url]['type'] = bs_object \
+                        .find("div",{"data-testid": "listing-summary-property-type"}) \
+                        .find("span") \
+                        .text
+            except:
+                property_metadata[property_url]['type'] = np.NaN
+
+            #extract rental bond($)
+            try:
+                property_metadata[property_url]["bond"] = re \
+                    .search(r"\d+",bs_object \
+                    .find("div",{"data-testid":"strip-content-list"}) \
+                    .findAll("li")[1].text) \
+                    .group()
+            except :
+                property_metadata[property_url]["bond"] = np.NaN
+
+            #extract property features
+            property_metadata[property_url]['features'] = [
+                feature.text for feature in bs_object \
+                    .findAll("li",{"data-testid": "listing-details__additional-features-listing"}) \
+            ]
+
+            #extract unverified features
+            property_metadata[property_url]["unverified_features"] = [
+                unverified.text.replace("*","") for unverified in bs_object \
+                    .findAll("li",{"data-testid": "listing-details__additional-features-suggested"}) \
+            ]
+            #extract property description
+            property_metadata[property_url]['desc'] = [
+                element.text for element in bs_object \
+                    .find("div",{"data-testid": "listing-details__description"}) \
+                    .findAll("p")
+            ]
 
 
 
-# output to example json in data/raw/
-with open('../data/raw/town_house.json', 'w') as f:
+# output to properties metadata as json file in data/raw_data/
+with open('../data/raw_data/properties.json', 'w') as f:
     dump(property_metadata, f)
